b524e9578ae08a411ffd7280f380b88f27181e1f
tomoya12 <peng4766@gmail.com>
2020-10-06 14:45:02 +0800

    fix some bug in js apply polyfill

--- code/howtojs/fn.apply.js
+++ code/howtojs/fn.apply.js
@@ -2,6 +2,7 @@ Function.prototype.myApply = function(context) {
   const context = context || window;
   context.fn = this;
   let result;
+  const args = [...arguments].slice(1);
   if (args[1]) {
     result = context.fn(...args[1]);
   } else {
